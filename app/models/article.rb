ce2401edb9a865fdfb459b03b4e0c41183adf828
Thomas Yager <tdy@andrew.cmu.edu>
2021-03-10 10:31:49 -0500

    Article model created and written

---
+++ app/models/article.rb
@@ -0,0 +1,11 @@
+class Article < ApplicationRecord
+
+    belongs_to :category
+
+    # title and content are required fields
+    validates_presence_of :title, :content
+
+    scope :active, 		 -> { where('active = ?', true) }
+    scope :alphabetical, -> { order('title') }
+
+end
